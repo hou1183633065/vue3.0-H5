ebe99c5c1100c4f31c6b3d501e9cd4fca215edec
houzhiqiang <zhq.hou@muheda.com>
2018-10-10 19:11:15 +0800

    add proxy

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,35 @@
 module.exports = {
-  css: undefined,
-
-  pwa: {
-    themeColor: "#349F6D"
+  css: {
+    sourceMap: true
   },
 
-  baseUrl: undefined,
-  outputDir: undefined,
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
+  pwa: {
+    themeColor: "#9F4034",
+    name: "helloWorld",
+    msTileColor: "#649C4C"
+  },
+
+  baseUrl: "/my-app/",
+  outputDir: "dist1",
+  assetsDir: "assets",
+  runtimeCompiler: true,
+  productionSourceMap: false,
   parallel: undefined,
-  lintOnSave: false
+  devServer: {
+    // 设置主机地址
+    host: "localhost",
+    // 设置默认端口
+    port: 8089,
+    // 设置代理
+    proxy: {
+      "/api": {
+        // 目标 API 地址
+        target: "http://idsw.muheda.com/IDSW",
+        // 如果要代理 websockets
+        ws: true,
+        // 将主机标头的原点更改为目标URL
+        changeOrigin: false
+      }
+    }
+  }
 };
