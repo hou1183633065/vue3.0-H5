a09e480ce834b0b5941743c107d4db4d3cf3ff45
houzhiqiang <zhq.hou@muheda.com>
2018-10-10 18:20:02 +0800

    公用函数

--- src/globalFun.js
+++ src/globalFun.js
@@ -1,5 +1,67 @@
 import Vue from "vue";
 
+// 判断是否为微信的运行环境
+Vue.prototype.isWeiXin = function() {
+  const ua = window.navigator.userAgent.toLowerCase();
+  if (ua.match(/MicroMessenger/i) == "micromessenger") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// 判断是否为小程序的运行环境
+Vue.prototype.isXiaoPro = function() {
+  if (this.isWeiXin()) {
+    return false;
+  } else {
+    if (window.__wxjs_environment === "miniprogram") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+// 全局截取URL参数函数
+// this.getParameter('uuid')
+Vue.prototype.getParameter = function(name) {
+  var urlStr = window.location.href;
+  // var urlStr = "?name=hou&id=5&page=1";
+  var reg = new RegExp("(^|\\?|&)" + name + "=([^&]*)(\\s|&|$)", "i");
+  var r = urlStr.substr(1).match(reg);
+  if (r != null) {
+    return unescape(r[2]);
+  } else {
+    return null;
+  }
+};
+
+// 获取客户ip
+// async onLoad() {
+//   var userLocation = await this.getUserLocation();
+//   console.log(userLocation);
+// }
+Vue.prototype.getUserLocation = function() {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement("script");
+    script.setAttribute("src", "https://pv.sohu.com/cityjson?ie=utf-8");
+    script.setAttribute("hidden", "hidden");
+    document.body.appendChild(script);
+    script.onload = script.onreadystatechange = function() {
+      if (
+        !this.readyState ||
+        this.readyState === "loaded" ||
+        this.readyState === "complete"
+      ) {
+        resolve(window.returnCitySN);
+        script.onload = script.onreadystatechange = null;
+      } else {
+        reject(null);
+      }
+    };
+  });
+};
 /*
 ** 时间戳转换成指定格式日期
 ** eg. 
